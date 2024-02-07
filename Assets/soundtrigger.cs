ea96c6ce53605da11e8ddf545eaae7b52c27de8f
kei13-beep <aditiasitorus13@gmail.com>
2024-02-07 12:27:23 +0700

    First Commit
    -Add Ball
    -Add Ground
    -Trigger Object Sound

---
+++ Assets/soundtrigger.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class soundtrigger : MonoBehaviour
+{
+   private void OnTriggerEnter(Collider other){
+    print("kena!");
+   }
+}
